c4e46efce98c95f8731c7cc9d327898e25d51823
Jebaitedneko <Jebaitedneko@gmail.com>
2021-11-15 14:26:35 +0530

    mm: allow oom_killer to be compiled out when DISABLE_OOM_KILLER=y
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1507,6 +1507,7 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 #endif
+#if !defined(CONFIG_DISABLE_OOM_KILLER)
 	{
 		.procname	= "panic_on_oom",
 		.data		= &sysctl_panic_on_oom,
@@ -1530,6 +1531,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#endif
 	{
 		.procname	= "overcommit_ratio",
 		.data		= &sysctl_overcommit_ratio,
