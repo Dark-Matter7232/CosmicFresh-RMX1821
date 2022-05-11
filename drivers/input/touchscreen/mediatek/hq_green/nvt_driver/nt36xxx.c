aed61bc6bdde1979d88c3ca979b7d77341846d01
Fei Mao <feim1@codeaurora.org>
2022-05-11 14:06:43 +0530

    input: touchscreen: nt36xxx: enter deep sleep state To avoid curret leakage, force touch IC to enter deep sleep state.
    [Jebaitedneko]: Modify cmds for SPI mode
    
    Change-Id: Icf6ef85806980e3881f0fabc3c0b47d49f0e6392
    Signed-off-by: Fei Mao <feim1@codeaurora.org>
    Co-authored-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- drivers/input/touchscreen/mediatek/hq_green/nvt_driver/nt36xxx.c
+++ drivers/input/touchscreen/mediatek/hq_green/nvt_driver/nt36xxx.c
@@ -1989,6 +1989,11 @@ static int32_t nvt_ts_suspend(struct device *dev)
 	buf[0] = EVENT_MAP_HOST_CMD;
 	buf[1] = 0x11;
 	CTP_SPI_WRITE(ts->client, buf, 2);
+
+		nvt_set_page(0x11a50);
+		buf[0] = 0x11a50 & 0xff;
+		buf[1] = 0x11;
+		CTP_SPI_WRITE(ts->client, buf, 2);
 #endif // WAKEUP_GESTURE
 
 	mutex_unlock(&ts->lock);
